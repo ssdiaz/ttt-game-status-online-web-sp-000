5cd14d93ec19621f5c7e6fd9253af79a863df4c1
Sam D <samidiaz3@gmail.com>
2021-01-08 22:46:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,9 +8,9 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
   [6,7,8], # Bottom row
-  [0,3,6], # Column 1 vertical
-  [1,4,7], # Column 2 vertical
-  [2,5,8], # Column 3 vertical
+  [0,3,6], # Column Left vertical
+  [1,4,7], # Column Middle vertical
+  [2,5,8], # Column Right vertical
   [0,4,8], # Right to Left diagonal
   [2,4,6]  # Left to Right diagonal
    ]
@@ -31,26 +31,12 @@ def won?(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
 
-    # win_index_4 = win_combination[3]
-    # win_index_5 = win_combination[4]
-    # win_index_6 = win_combination[5]
-    # win_index_7 = win_combination[6]
-    # win_index_8 = win_combination[7]
-    # win_index_9 = win_combination[8]
-
     position_1 = board[win_index_1] # load the value of the board at win_index_1
     position_2 = board[win_index_2] # load the value of the board at win_index_2
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
-    # position_4 = board[win_index_4] # load the value of the board at win_index_1
-    # position_5 = board[win_index_5] # load the value of the board at win_index_2
-    # position_6 = board[win_index_6] # load the value of the board at win_index_3
-    # position_7 = board[win_index_7] # load the value of the board at win_index_1
-    # position_8 = board[win_index_8] # load the value of the board at win_index_2
-    # position_9 = board[win_index_9] # load the value of the board at win_index_3
-
-#
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  #  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  if position_1 ==  position_2 == position_3 == "X" || position_1 ==  position_2 == position_3 == "O"
       return win_combination # return the win_combination indexes that won.
     else
       false
