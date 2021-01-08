6b39cac6ef93598304c7689ae2ef178989ab6468
Sam D <samidiaz3@gmail.com>
2021-01-08 22:20:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,10 +45,23 @@ WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-#
+    win_index_4 = win_combination[3]
+    win_index_5 = win_combination[4]
+    win_index_6 = win_combination[5]
+    win_index_7 = win_combination[6]
+    win_index_8 = win_combination[7]
+    win_index_9 = win_combination[8]
+
     position_1 = board[win_index_1] # load the value of the board at win_index_1
     position_2 = board[win_index_2] # load the value of the board at win_index_2
     position_3 = board[win_index_3] # load the value of the board at win_index_3
+    position_4 = board[win_index_4] # load the value of the board at win_index_1
+    position_5 = board[win_index_5] # load the value of the board at win_index_2
+    position_6 = board[win_index_6] # load the value of the board at win_index_3
+    position_7 = board[win_index_7] # load the value of the board at win_index_1
+    position_8 = board[win_index_8] # load the value of the board at win_index_2
+    position_9 = board[win_index_9] # load the value of the board at win_index_3
+
 #
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination # return the win_combination indexes that won.
@@ -56,6 +69,5 @@ WIN_COMBINATIONS.each do |win_combination|
       false
     end
   end
-
 
 end
