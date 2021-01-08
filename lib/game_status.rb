594cd8dcb7271d7cb5e5db356f3054d812cea579
Sam D <samidiaz3@gmail.com>
2021-01-08 21:50:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,11 +23,9 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
   [6,7,8], # Bottom row
-
   [0,3,6], # Column 1 vertical
   [1,4,7], # Column 2 vertical
   [2,5,8], # Column 3 vertical
-
   [0,4,8], # Right to Left diagonal
-  [2,4,6] # Left to Right diagonal
+  [2,4,6]  # Left to Right diagonal
    ]
