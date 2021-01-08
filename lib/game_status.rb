f3f48fa8e61dd6fd47db379ff0ab5f72c568dc1f
Sam D <samidiaz3@gmail.com>
2021-01-08 22:47:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,8 +36,10 @@ def won?(board)
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
   #  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-  if position_1 ==  position_2 == position_3 == "X"
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination # return the win_combination indexes that won.
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination # return the win_combination indexes that won.
     else
       false
     end
