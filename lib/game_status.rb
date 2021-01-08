2441221d985e8bce9106412f0d2d61d9c8f6f65a
Sam D <samidiaz3@gmail.com>
2021-01-08 23:03:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,8 +39,6 @@ def won?(board)
   if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination # return the win_combination indexes that won.
     else
-      #false
-      position_taken?(board, index) == false
       false
     end
     # if position_taken == false , then fail?????/
