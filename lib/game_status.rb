fd1db96312dd9694a4a14f6a7e6538d7d2a6cb62
Sam D <samidiaz3@gmail.com>
2021-01-08 22:29:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,21 +4,6 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-
-
-# build #WON?
-# 8 possible ways to win: 3 horizontal, 3 vertical columns, 2 diagonals
-# each win combo should be represented by 3 elements array referring to index won
-# board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-
-#win_top_row = board[0,1,2]
-
-#if board[top_row_win[0]] == "X"
-#   && board[top_row_win[1]] == "X"
-#   && board[top_row_win[2]] == "X"
-#  "X won in the top row"
-#end
-
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
@@ -39,7 +24,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
 #  for each win_combination in WIN_COMBINATIONS
-WIN_COMBINATIONS.each do |win_combination|
+#WIN_COMBINATIONS.each do |win_combination|
+  for each win_combination in WIN_COMBINATIONS
     # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
     # grab each index from the win_combination that composes a win.
     win_index_1 = win_combination[0]
