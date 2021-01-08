b60ddab22ca188825d94df95dfaf0668b380191c
Sam D <samidiaz3@gmail.com>
2021-01-08 21:56:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,3 +29,27 @@ WIN_COMBINATIONS = [
   [0,4,8], # Right to Left diagonal
   [2,4,6]  # Left to Right diagonal
    ]
+
+# #won? method. should accept board as an agument.
+# returns false/nil if there is no winning combo and return winning array
+# does not return who won, only how (the index #s)
+# iterate the possible win combos to check if the boad has same player toke in
+#      each index of a winning combo
+
+for each win_combination in WIN_COMBINATIONS
+  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+  # grab each index from the win_combination that composes a win.
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination # return the win_combination indexes that won.
+  else
+    false
+  end
+end
