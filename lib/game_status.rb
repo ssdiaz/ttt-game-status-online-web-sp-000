45d8e9b0eebf5e295cc30efcaccf51b9d0b2fc30
Sam D <samidiaz3@gmail.com>
2021-01-08 21:56:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,21 +35,3 @@ WIN_COMBINATIONS = [
 # does not return who won, only how (the index #s)
 # iterate the possible win combos to check if the boad has same player toke in
 #      each index of a winning combo
-
-for each win_combination in WIN_COMBINATIONS
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1] # load the value of the board at win_index_1
-  position_2 = board[win_index_2] # load the value of the board at win_index_2
-  position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination # return the win_combination indexes that won.
-  else
-    false
-  end
-end
