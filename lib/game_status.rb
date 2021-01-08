37df528ab7e18fcf2e2ebb3cbcd3ca7fa73c2d32
Sam D <samidiaz3@gmail.com>
2021-01-08 21:49:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,30 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+
+# build #WON?
+# 8 possible ways to win: 3 horizontal, 3 vertical columns, 2 diagonals
+# each win combo should be represented by 3 elements array referring to index won
+# board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
+#win_top_row = board[0,1,2]
+
+#if board[top_row_win[0]] == "X"
+#   && board[top_row_win[1]] == "X"
+#   && board[top_row_win[2]] == "X"
+#  "X won in the top row"
+#end
+
+WIN_COMBINATIONS = [
+  [0,1,2] # Top row
+  [3,4,5] # Middle row
+  [6,7,8] # Bottom row
+
+  [0,3,6]# Column 1 vertical
+  [1,4,7]# Column 2 vertical
+  [2,5,8]# Column 3 vertical
+
+  [0,4,8]# Right to Left diagonal
+  [2,4,6]# Left to Right diagonal
+   ]
