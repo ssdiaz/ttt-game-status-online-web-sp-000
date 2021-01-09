c8d6776fca816664198bc079a2ec8ea9e47d65df
Sam D <samidiaz3@gmail.com>
2021-01-09 00:40:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,7 +58,6 @@ def winner(board)
   if won?(board)
 # find winning index inside of board and display it
     winner_index_1 = win_combo[0]
-
     position_1 = board[winner_index_1] # load the value of the board at win_index_1
 
     if position_1 == "X"
