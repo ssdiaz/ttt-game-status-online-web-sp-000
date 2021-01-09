5f77b9c9b39145ee82679897031fbc56d0cd996b
Sam D <samidiaz3@gmail.com>
2021-01-09 00:17:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -57,7 +57,7 @@ def winner(board)
 # returns the token that won
   #puts won?(board).find {|win_combination| win_combination == "X" || win_combination == "O"}
   if won?(board)
-    win_combination.find {|win_combination| win_combination == "X" || win_combination == "O"}
+    won?(board).find {|win_combination| win_combination == "X" || win_combination == "O"}
   end
 
 end
